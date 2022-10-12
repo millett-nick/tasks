620f790b9198a44e8c1297a08f1c4ec81b282d4a
millett-nick <nickolas.millett@maine.edu>
2022-10-11 22:53:26 -0400

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
 import React from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
+import { reduceEachLeadingCommentRange } from "typescript";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-<<<<<<< HEAD
-            <header className="App-header"> COS420, Nickolas Millett</header>
-            <p>Hello World</p>
-=======
-            <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save to reload.
-            </p>
->>>>>>> upstream/task-functions
-        </div>
+        <>
+            <div className="App">
+                <h1>UM COS 420 Nickolas Milllett</h1>
+                <header className="App-header">
+                    UM COS420 Nickolas Milllett
+                </header>
+                <p>Hello World!</p>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "400px",
+                                    height: "10px",
+                                    backgroundColor: "red"
+                                }}
+                            ></div>
+                            First Column. My list:
+                            <ul>
+                                <li>fish</li>
+                                <li>dog</li>
+                                <li>cat</li>
+                            </ul>
+                        </Col>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "400px",
+                                    height: "10px",
+                                    backgroundColor: "red"
+                                }}
+                            ></div>
+                            Second Column
+                            <img src=".../Images/fishman.png" alt="is fish" />
+                        </Col>
+                        <Button onClick={() => console.log("Hello World!")}>
+                            Log Hello World
+                        </Button>
+                    </Row>
+                </Container>
+            </div>
+        </>
     );
 }
 
