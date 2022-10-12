8aaf22509b142881777b6206dc47dabdaaf562aa
millett-nick <nickolas.millett@maine.edu>
2022-10-11 22:26:06 -0400

    added colums

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">UM COS420 Nickolas Milllett</header>
-            <p>Hello</p>
-            My list:
-            <ul>
-                <li>fish</li>
-                <li>dog</li>
-                <li>cat</li>
-            </ul>
-            <img src=".../Images/fishman.png" alt="is fish" />
-            <Button onClick={() => console.log("Hello World")}>
-                Log Hello World
-            </Button>
-        </div>
+        <>
+            <div className="App">
+                <header className="App-header">
+                    UM COS420 Nickolas Milllett
+                </header>
+                <p>Hello</p>
+                <Container>
+                    <Row>
+                        <Col>
+                            First Column. My list:
+                            <ul>
+                                <li>fish</li>
+                                <li>dog</li>
+                                <li>cat</li>
+                            </ul>
+                        </Col>
+                        <Col>
+                            Second Column
+                            <img src=".../Images/fishman.png" alt="is fish" />
+                        </Col>
+                        <Button onClick={() => console.log("Hello World")}>
+                            Log Hello World
+                        </Button>
+                    </Row>
+                </Container>
+            </div>
+        </>
     );
 }
 
