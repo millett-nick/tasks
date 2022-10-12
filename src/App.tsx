feb1e6be23c3de2348b29f4692912c6bd56a0679
millett-nick <nickolas.millett@maine.edu>
2022-10-11 21:36:36 -0400

    fixed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header"> COS420, Nickolas Millett</header>
+            <p>Hello World</p>
+        </div>
+    );
+}
+
+export default App;
