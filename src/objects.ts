d5216b9d9ffd25ce2373248f3d01b2b4429ac693
millett-nick <nickolas.millett@maine.edu>
2022-10-21 12:04:33 -0400

    short form done

--- src/objects.ts
+++ src/objects.ts
@@ -58,7 +58,8 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const cutname = question.name.slice(0, 10);
+    return question.id + ": " + cutname;
 }
 
 /**
