ec1ac9ae644e9acb515f20e2fcb4527095ec8758
millett-nick <nickolas.millett@maine.edu>
2022-10-24 17:42:25 -0400

    toCSV done

--- src/nested.ts
+++ src/nested.ts
@@ -137,7 +137,18 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const deepCopy = questions.map(
+        (question: Question): Question => ({ ...question })
+    );
+    console.log(deepCopy);
+    const CSV = deepCopy
+        .map(
+            (question: Question): string =>
+                `${question.id},${question.name},${question.options.length},${question.points},${question.published}`
+        )
+        .join("\n");
+
+    return "id,name,options,points,published\n" + CSV;
 }
 
 /**
