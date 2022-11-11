0ed9d99090fc84d49207e7312fd24e4b61a65dda
millett-nick <nickolas.millett@maine.edu>
2022-11-11 18:40:40 -0500

    colored box and shove box done

--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,20 +1,16 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-
-function ShoveBoxButton({
-    position,
-    setPosition
-}: {
-    position: number;
+interface props {
     setPosition: (newPosition: number) => void;
-}) {
+    position: number;
+}
+function ShoveBoxButton({ position, setPosition }: props) {
     return (
         <Button onClick={() => setPosition(4 + position)}>Shove the Box</Button>
     );
 }
 
-function MoveableBox(): JSX.Element {
-    const [position, setPosition] = useState<number>(10);
+function MoveableBox({ position, setPosition }: props): JSX.Element {
     return (
         <div
             data-testid="moveable-box"
@@ -32,19 +28,22 @@ function MoveableBox(): JSX.Element {
 }
 
 export function ShoveBox(): JSX.Element {
-    const box = MoveableBox();
-
+    // const box = MoveableBox();
+    const [position, setPosition] = useState<number>(10);
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            <span>The box is at: {position}</span>
             <div>
                 <ShoveBoxButton
-                    position={box.position}
-                    setPosition={box.setPosition}
+                    position={position}
+                    setPosition={setPosition}
                 ></ShoveBoxButton>
-                {box}
-            </div> */}
+                <MoveableBox
+                    position={position}
+                    setPosition={setPosition}
+                ></MoveableBox>
+            </div>
         </div>
     );
 }
